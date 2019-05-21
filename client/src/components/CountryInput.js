3ae4fded84dc8d3afa3d36fa93ae4968535c159a
notpluto <srijan.kpr@gmail.com>
2019-05-21 17:07:27 +0530

    handle signup

--- client/src/components/CountryInput.js
+++ client/src/components/CountryInput.js
@@ -4,9 +4,7 @@ class CountryInput extends React.Component {
 	render() {
 		return(
 			<div className="country-input hero">
-				<fieldarea>
-					<input type="text" placeholder="Where do you want to go today?" name="countryinput"/>
-				</fieldarea>
+				<input type="text" placeholder="Where do you want to go today?" name="countryinput"/>
 			</div>
 		)
 	}
--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,13 +1,53 @@
 import React, { Component } from 'react';
 
 class SignUp extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+        username: "",
+        email: "",
+        password: "",
+        confirmpassword: "",
+    }
+  }
+
+  handleSignup = (e) => {
+    e.preventDefault()
+    console.log('inside signup')
+    if(this.state.password === this.state.confirmpassword) {
+      fetch('/api/users/register', {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: this.state.username,
+          email: this.state.email,
+          password: this.state.password,
+        }), 
+      }).then(res => console.log(res))
+    }
+  }
+
+  handleChange = (e) => {
+    const {name, value} = e.target
+    this.setState(
+      {[name]: value}
+    )
+  }
+
+  componentDidMount() {
+
+  }
+
   render() {
     return (
-      <form action="" method="post" >
-      	<input type="text" name="name" placeholder="Username" required />
-      	<input type="email" name="email" placeholder="Email address" required />
-      	<input type="password" name="password" placeholder="Password" required />
-        <input type="password" name="password" placeholder="Confirm password" required />
+      <form onSubmit= {this.handleSignup} >
+      	<input type="text" name="username" placeholder="Username" value={this.state.username} onChange= {this.handleChange} required />
+      	<input type="email" name="email" placeholder="Email address" value={this.state.email} onChange= {this.handleChange} required />
+      	<input type="password" name="password" placeholder="Password" value={this.state.password} onChange= {this.handleChange} required />
+        <input type="password" name="confirmpassword" placeholder="Confirm password" value={this.state.confirmpassword} onChange= {this.handleChange} required />
         { /* <input type="file" name="photo" />*/ }
       	<input type="submit" value="login" />
       </form>
