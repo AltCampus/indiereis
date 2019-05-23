36c7560f09f2cd23de7edb57fafba02b52501f91
sajan <sajanloveu123@gmail.com>
2019-05-23 11:58:34 +0530

    pulling login app js condition

--- server/models/Data.js
+++ server/models/Data.js
@@ -3,20 +3,10 @@ var Schema = mongoose.Schema;
 const timestamp = require('time-stamp');
 
 var dataSchema = new Schema({
-	questions : [{
+	userData : [{
 		type: Object,
 		default: ""
 	}],
-	answers: [{
-		type: Object,
-		default: ""
-	}],
-
-	comments: {
-		type: String,
-		default: ""
-	}
-
 }, {timestamps: true});
 
 
