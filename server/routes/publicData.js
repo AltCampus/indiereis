2a82eec0f2de3b78c7bad2a464b5dc8ce5bc9ec4
sajan <sajanloveu123@gmail.com>
2019-05-25 12:41:30 +0530

    add options in question schema

--- server/routes/publicData.js
+++ server/routes/publicData.js
@@ -29,4 +29,20 @@ router.post('/', (req,res,next) => {
 	})
 })
 
+router.get('/:id', (req,res,next) => {
+	var id = req.params.id;
+	console.log("inside userData get...");
+	UserData.findOneAndDelete({ _id: id }, ( err, data ) => {
+		if(err) return res.status(500).json({ success: false, message: "server side error"});
+		console.log(data, "data deleted ...");
+		if(data) {
+			User.findOneAndUpdate({ _id: data.userId },{ $pull: { userData: data.user._id } }, (err, user) => {
+			if(err) return next(err);
+				console.log(user, "user updated with his info...");
+				res.status(200).json({ success: true });
+			}
+		)}
+	})
+})
+
 module.exports = router;
